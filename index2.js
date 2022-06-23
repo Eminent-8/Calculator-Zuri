39f3f5d96a1b9283d62f7311e6b551c373a16998
Eminent-8 <chizuberejoy@gmail.com>
2022-06-23 14:27:37 +0100

    any message here

---
+++ index2.js
@@ -0,0 +1,22 @@
+var Opr = prompt("Operator");
+
+var num1 = parseFloat(prompt("number1: "));
+var num2 = parseFloat(prompt("number2: "));
+
+
+if (Opr == "+") 
+{
+     console.log(num1 + num2); 
+}
+else if (Opr == "-") 
+{
+    console.log(num1 - num2); 
+}
+else if (Opr == "*") 
+{
+    console.log(num1 * num2); 
+}
+else if (Opr == "/") 
+{
+   console.log(num1 / num2); 
+}
